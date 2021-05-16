40d094dedbe0c76ef3f8c0d6787aef5a69a2de66
arunwandale <84247687+arunwandale@users.noreply.github.com>
2021-05-16 13:49:01 +0530

    Update app.py
    applied ngrok module

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 run_with_ngrok(app)
 
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = '*××*'
+app.config['MYSQL_USER'] = '****'
 app.config['MYSQL_PASSWORD'] = '****'
 app.config['MYSQL_DB'] = '****'
 
